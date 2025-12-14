598edc95c863ebf8ba36a057d1f3aad070bfde37
Sai Venkata Teja <saivenkatateja714@gmail.com>
2025-12-14 12:59:48 +0530

    Kafka Config and Kafka Producer COnfiguration Setup

---
+++ src/main/java/com/event/kafka/kafka/KafkaProducer.java
@@ -0,0 +1,18 @@
+package com.event.kafka.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message){
+        kafkaTemplate.send("practicekafka1",message);
+    }
+}
